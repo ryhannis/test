cc73075f6f13ef65f800faff08c1dcc8fa15ccdc
ryhannis <ryans1995@gmail.com>
2020-10-22 23:38:02 +1100

    second commit

--- single.php
+++ single.php
@@ -12,7 +12,7 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-	next_post_link( string $format = '%link &raquo;', string $link = '%title', bool $in_same_term = false, array|string $excluded_terms = '', string $taxonomy = 'category' )
+	
 
 		<?php
 		while ( have_posts() ) :
@@ -26,16 +26,17 @@ get_header();
 		
 <div class="prev items-center center absolute">
 	<div class="pagination-inner">
-	<?php previous_post_link()?>
+	<?php previous_post_link('%link', '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 53.87 96.61"><defs><style>.cls-1{fill:none;stroke:#707070;stroke-width:3px;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path id="Path_275" data-name="Path 275" class="cls-1" d="M52.85,1.1,2.2,48.28,52.85,95.51"/></g></g></svg>');?>
 	</div>
 </div>
 
 <div class="next items-center cente absolute">
 	<div class="pagination-inner">
-	<?php next_post_link()?>
+	<?php next_post_link('%link', '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 53.87 96.61"><defs><style>.cls-1{fill:none;stroke:#707070;stroke-width:3px;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path id="Path_275" data-name="Path 275" class="cls-1" d="M1,95.51,51.67,48.33,1,1.1"/></g></g></svg>');?>
 	
 	</div>
 </div>
+
 
 
 
